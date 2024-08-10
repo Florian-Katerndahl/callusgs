63e18c0a472c6f0fa8b867b4bf7c76f3d4e84cb1
Florian-Katerndahl <florian@katerndahl.com>
2024-08-10 11:27:40 +0200

    add cli module to possible imports

--- callusgs/__init__.py
+++ callusgs/__init__.py
@@ -3,5 +3,6 @@ import callusgs.types as Types
 import callusgs.errors as Errors
 from callusgs.utils import ogr2internal
 from callusgs.exits import ExitCodes
+from callusgs import cli
 
-__all__ = ["Api", "Types", "Errors", "ogr2internal", "ExitCodes"]
+__all__ = ["Api", "Types", "Errors", "ogr2internal", "ExitCodes", "cli"]
